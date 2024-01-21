60e6db606f1ec796df7d87625521c1a6121c2165
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2024-01-21 09:14:43 +0530

    Hi

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from PIL import Image
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    width = int(request.form['width'])
+    height = int(request.form['height'])
+    image_file = request.files['file']
+    filename = secure_filename(image_file.filename)
+    image_file.save(os.path.join('static/', filename))
+    image = Image.open(os.path.join('static/', filename))
+    image.thumbnail((width, height))
+    image.save(os.path.join('static/', filename))
+    return render_template('upload.html', filename=filename)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename=filename))
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port='81')
